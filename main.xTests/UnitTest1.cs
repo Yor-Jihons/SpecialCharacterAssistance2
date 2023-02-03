753b79cf4962ab932f251d661983125e77735dbf
Yor-Jihons <ryo.malon814@gmail.com>
2023-02-03 15:07:25 +0900

    テストの追加

--- main.xTests/UnitTest1.cs
+++ main.xTests/UnitTest1.cs
@@ -5,8 +5,26 @@ namespace main.xTests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void SpecialCharacterTest()
     {
+        // TODO: Test for ClassMappings.SpecialCharacter
+    }
 
+    [Fact]
+    public void GenreTest()
+    {
+        // TODO: Test for ClassMappings.Genre
+    }
+
+    [Fact]
+    public void SpecialCharactersTest()
+    {
+        // TODO: Test for ClassMappings.SpecialCharacters
+    }
+
+    [Fact]
+    public void ReplacerTest()
+    {
+        // TODO: Test for Replacers.Replacer
     }
 }
